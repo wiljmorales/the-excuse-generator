8a68001e56fe06b982a27b59885d4a9090ce7f25
Wilfredo Morales <wiljmoralesa@gmail.com>
2022-05-03 23:56:42 -0400

    Challenge Complete

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird", "My mon"];
+  let action = ["ate", "peed", "crushed", "broke", "cook"];
+  let what = ["my homework", "the keys", "the car", "my repository", "my code"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let randomNumber = Math.floor(Math.random() * 5);
+
+  document.querySelector(
+    "#excuse"
+  ).innerHTML = `${who[randomNumber]} ${action[randomNumber]} ${what[randomNumber]} ${when[randomNumber]}`;
 };
+
